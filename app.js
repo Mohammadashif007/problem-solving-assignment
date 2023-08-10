76cc799f1bef8fd2fc4278535c0505e2209b3c18
Mohammad Ashif <mohammadashif510@gmail.com>
2023-08-10 15:31:29 +0600

    edit done

--- app.js
+++ app.js
@@ -23,32 +23,32 @@
 
 // 3. Third problem
 
-function sortMaker(arr){
-    let largestSequence = [];
-    for(let i = 0; i < arr.length; i++){
-        if(arr[i] > arr[i+1] && arr[i] >= 0 && arr[i+1] >= 0){
-            largestSequence.push(arr[i]);
-            largestSequence.push(arr[i+1]);
-            break
+// function sortMaker(arr){
+//     let largestSequence = [];
+//     for(let i = 0; i < arr.length; i++){
+//         if(arr[i] > arr[i+1] && arr[i] >= 0 && arr[i+1] >= 0){
+//             largestSequence.push(arr[i]);
+//             largestSequence.push(arr[i+1]);
+//             break
 
-        }
-        else if(arr[i] == arr[i+1]){
-            return "equal";
-        }
-        else if(arr[i] < 0 || arr[i+1] < 0){
-            return "Invalid Input";
-        }
-        else {
-            largestSequence.push(arr[i+1]);
-            largestSequence.push(arr[i]);
-            break
-        }
-    }
-    return largestSequence;
+//         }
+//         else if(arr[i] == arr[i+1]){
+//             return "equal";
+//         }
+//         else if(arr[i] < 0 || arr[i+1] < 0){
+//             return "Invalid Input";
+//         }
+//         else {
+//             largestSequence.push(arr[i+1]);
+//             largestSequence.push(arr[i]);
+//             break
+//         }
+//     }
+//     return largestSequence;
 
-}
+// }
 
-console.log(sortMaker([4, 10]));
+// console.log(sortMaker([4, 10]));
 
 // 4. Fourth problem
 
